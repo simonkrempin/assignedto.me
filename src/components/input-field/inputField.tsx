3cd9900b16bcad4e8eace8c1fbea28e50ce14001
Hintemann, Niklas <niklas.hintemann@d-velop.de>
2023-05-26 11:51:54 +0200

    final input field

--- src/components/input-field/inputField.tsx
+++ src/components/input-field/inputField.tsx
@@ -1,20 +1,36 @@
-"use client";
-
 import react from "react";
 
 import "./style.css";
 
-type InputMode = "user" | "keineAhnung";
+type InputMode = "large" | "small";
 
 interface inputProps {
-    children: React.ReactNode;
-    InputMode: "inputMode";
+    label: string;
+    text: string;
+    changeText: (text: string) => void;
+    inputMode: InputMode;
 }
 
-const InputField = () => {
-    return <div>
-        <input type="text"></input>
-    </div>; 
+const InputField = ({ text, inputMode, label, changeText }: inputProps) => {
+    const onChange = (event: react.ChangeEvent<HTMLInputElement>) => {
+        changeText(event.target.value);
+    }
+
+    switch (inputMode) {
+        case "large":
+            return (
+                <div>
+                    <label>{label}</label>
+                    <input type="text" value={text} onChange={onChange}></input>
+                </div>
+            );
+        case "small":
+            return (
+                <p contentEditable={true} onChange={onChange}>{text}</p>
+            );
+        default:
+            throw new Error("not implemented");
+    }
 };
 
 export { InputField };
