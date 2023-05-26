15c9bc1435d71ae35dd24b8aa2ad99ccddda1b97
Hintemann, Niklas <niklas.hintemann@d-velop.de>
2023-05-26 11:15:52 +0200

    Input Field

--- src/components/input-field/inputField.tsx
+++ src/components/input-field/inputField.tsx
@@ -1,7 +1,20 @@
+"use client";
+
 import react from "react";
 
+import "./style.css";
+
+type InputMode = "user" | "keineAhnung";
+
+interface inputProps {
+    children: React.ReactNode;
+    InputMode: "inputMode";
+}
+
 const InputField = () => {
-    return <div>input field</div>;
+    return <div>
+        <input type="text"></input>
+    </div>; 
 };
 
 export { InputField };
