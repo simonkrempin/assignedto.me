84e9d97a60eb33f718184e6ed7db4f00a9ab3364
Dreamonix <128577740+Dreamonix@users.noreply.github.com>
2023-05-27 15:48:48 +0200

    created button -option small/large

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,7 +1,28 @@
 import react from "react";
 
-const Button = () => {
-    return <div>button</div>;
+import "./style.css";
+
+type Mode = "large" | "small";
+
+interface inputProps {
+    mode : Mode;
+    text: string;
+}
+
+
+
+const Button = ({text,mode}: inputProps) => {
+    switch(mode) {
+        case "large":
+            return (
+                <button className=" btn largeButton">{text}</button>
+            );
+        case "small":
+            return (
+                <button className=" btn smallButton">{text}</button>
+            )
+    }
+    
 };
 
 export { Button };
