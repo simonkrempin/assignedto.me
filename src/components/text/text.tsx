036044fe42bed898777ee13c31ec1fa2662f100f
Simon Krempin <67335158+simonkrempin@users.noreply.github.com>
2023-05-27 10:25:11 +0200

    textstyling (#4)
    Co-authored-by: Mara Hübers <mara.huebers@flender.com>

--- src/components/text/text.tsx
+++ src/components/text/text.tsx
@@ -1,7 +1,23 @@
 import react from "react";
 
-const Text = () => {
-    return <div>text</div>;
-};
+type TextMode = "name" |"button"|"title"| "discription";
+
+interface TextProps {
+    children: React.ReactNode;
+    mode: TextMode;
+}
+
+const Text = ({ children, mode = "discription" }: TextProps) => {
+    switch (mode) {
+        case "name": 
+            return <h1>{children}</h1>
+        case "button":
+            return <h2>{children}</h2>
+        case "title":
+            return <h3>{children}</h3>
+        default:
+            return <p>{children}</p>
+    }
+}
 
 export { Text };
