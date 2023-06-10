4c571316128ddcfd7dd933eee3835839e100016d
Dreamonix <128577740+Dreamonix@users.noreply.github.com>
2023-06-10 21:09:58 +0200

    sidebar Komponente - ohne selected function ?

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
+import { Button } from "@/components";
+import { Calender } from "@/components";
 
-const Sidebar = () => {
-    return <div>sidebar</div>;
+interface Props {
+    title: String;
+}
+
+const Sidebar = ({title} : Props) => {
+    
+    // Function für selected, welche die entsprechende Filterlogik implementiert, basierend auf dem ausgewählten Punkt
+
+    return <div>
+        <div>
+            <h1>{title}</h1>
+        </div>
+        <div className="btnGroup">
+            <Button size="large" color="lighter" selected={} onClick={} >
+                Alle
+            </Button>
+            <Button size="large" color="lighter" selected={} onClick={} >
+                Aufgaben
+            </Button>
+            <Button size="large" color="lighter" selected={} onClick={} >
+                Aufgegeben
+            </Button>
+        </div>
+        <div className="calender">
+            <Calender></Calender>
+        </div>
+        <div className="linkGroup">
+            <Button size="small" color="lighter" selected={} onClick={} >
+                icon Einstellungen
+            </Button>
+            <Button size="small" color="lighter" selected={} onClick={} >
+                icon Abmelden
+            </Button>
+        </div>
+    </div>;
 };
 
 export default Sidebar;
