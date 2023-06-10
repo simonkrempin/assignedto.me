be3bb5d86f5087a8e49dc8af5d542f85583b2a3c
Dreamonix <128577740+Dreamonix@users.noreply.github.com>
2023-06-10 20:40:33 +0200

    rework button component

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,32 +1,38 @@
-import react from "react";
+import React, {ReactNode} from "react";
 
 import "./style.css";
 
-type Mode = "large" | "small" | "link";
+type Size = "large" | "small";
 
 interface inputProps {
-    mode : Mode;
-    text: string;
+    size : Size;
+    color: "lighter" | "darker" 
+    selected: boolean;
+    onClick: () => void;
+    children: ReactNode; // Added children prop
 }
 
-
-
-const Button = ({text,mode}: inputProps) => {
-    switch(mode) {
+const Button = ({size, color, selected, onClick, children}: inputProps ) => {
+    const buttonClassName = size === 'large' ? 'btnLarge' : 'btnSmall';
+    
+    switch (size) {
         case "large":
             return (
-                <button className="largeBtn">{text}</button>
+                // For large Button selected function should change the color prop to darker
+                <button className={`${buttonClassName} ${color}`}   onClick={onClick}>
+                    {children}
+                </button>
             );
         case "small":
             return (
-                <button className="smallBtn">{text}</button>
-            );
-        case "link":
-            return (
-                <button className="linkBtn">{text}</button>
+                // Small Button is for links only as settings, login, register etc.
+                // Do not use darker color, as it adds background and a border to the button
+                <button className={`${buttonClassName} ${color}`} onClick={onClick}>
+                    {children}
+                </button>
             )
+
     }
-    
-};
+}
 
 export { Button };
